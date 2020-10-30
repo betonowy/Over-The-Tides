d3d0a1d21229214e95099d55103ec6e0f3c489d4
betonbeton <57034426+betonbeton@users.noreply.github.com>
2020-10-30 22:19:47 +0100

    Useful functions added, not used yet
    Determine enemy ship orientation/position against player

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
@@ -20,6 +21,9 @@ public class EnemyScript : MonoBehaviour {
 
     public GameObject projectile;
 
+    private GameObject meTheEnemy;
+    private Rigidbody2D myBody;
+
 
 
     // Start is called before the first frame update
@@ -27,6 +31,8 @@ public class EnemyScript : MonoBehaviour {
     void Start() {
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        meTheEnemy = GetComponent<GameObject>();
+        myBody = GetComponent<Rigidbody2D>();
 
         timeBtwShots = startTimeBtwShots;
 
@@ -42,14 +48,11 @@ public class EnemyScript : MonoBehaviour {
 
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
 
-        }
-        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreaDistance) {
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreaDistance) {
 
             transform.position = this.transform.position;
 
-        }
-
-        else if (Vector2.Distance(transform.position, player.position) > retreaDistance) {
+        } else if (Vector2.Distance(transform.position, player.position) > retreaDistance) {
 
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
 
@@ -63,13 +66,33 @@ public class EnemyScript : MonoBehaviour {
 
             timeBtwShots = startTimeBtwShots;
 
-        }
-        else {
+        } else {
 
             timeBtwShots -= Time.deltaTime;
 
         }
 
+    }
+
+    Vector2 getVectorToPlayer() {
+        return player.transform.position - meTheEnemy.transform.position;
+    }
+
+    Vector2 getMyDirection() {
+        return new Vector2(Mathf.Sin(myBody.rotation), Mathf.Cos(myBody.rotation)); ;
+    }
+
+    Vector2 getRightHandDirection() {
+        Vector2 twist = getMyDirection();
+        return new Vector2(twist.y, -twist.x);
+    }
+
+    float scalarTowardsTarget() {
+        return Vector2.Dot(getMyDirection(), getVectorToPlayer().normalized);
+    }
+
+    float scalarRightHandTowardsTarget() {
+        return Vector2.Dot(getRightHandDirection(), getVectorToPlayer().normalized);
     }
 }
 
