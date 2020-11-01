796cd1d25523d0cf778670bce9fcc9ff967f32ad
betonbeton <57034426+betonbeton@users.noreply.github.com>
2020-11-01 20:55:35 +0100

    Fix background texture offset

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 public class BackgroundScript : MonoBehaviour {
     MeshRenderer mr;
+    public GameObject cam;
 
     private void Start() {
         //transform.localScale = new Vector3(transform.localScale.x * 2f, transform.localScale.y * 2f, 0);
         mr = GetComponent<MeshRenderer>();
         //mr.sortingLayerName
         mr.sortingOrder = 0;
+        cam = GameObject.Find("Main Camera");
     }
     private void Update() {
-        
-  
         Material mat = mr.material;
         Vector2 offset = mat.mainTextureOffset;
-        offset.y += Time.deltaTime / 2;
+        offset = cam.transform.position;
         mat.mainTextureOffset = offset;
 
     }
