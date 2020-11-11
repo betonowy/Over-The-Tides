f6d123910200162ac0fbc44d51b0303ee7893b9d
mentalsquat <helloscr666@gmail.com>
2020-11-11 21:47:05 +0100

    Working Menu-Game System

--- Assets/Scripts/Main Menu/MainManu.cs
+++ Assets/Scripts/Main Menu/MainManu.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainManu : MonoBehaviour
 {
     public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void PlayTutorialGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame() {
