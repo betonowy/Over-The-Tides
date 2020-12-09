a26405ff83108564b48adbacd79f89432f2f09ad
mentalsquat <helloscr666@gmail.com>
2020-12-09 18:57:18 +0100

    Timer desync fix

--- Assets/Scripts/UI Elements/Timer/TimerScript.cs
+++ Assets/Scripts/UI Elements/Timer/TimerScript.cs
@@ -13,24 +13,11 @@ public class TimerScript : MonoBehaviour
 
     public HordeMan hordeMan;
 
-    private void Start() {
+    private void Update() {
         countdownTime = hordeMan.GetCountdown();
-        StartCoroutine(CountdownToStart());
-    }
-
-    IEnumerator CountdownToStart() {
-        while (true) {
-            if (countdownTime >= 0) {
-                TimeSpan t = TimeSpan.FromSeconds(countdownTime);
-                countdownDisplay.text = t.Minutes + ":" + t.Seconds;
-
-                yield return new WaitForSeconds(1f);
-                countdownTime--;
-            } else {
-                countdownTime = hordeMan.GetCountdown();
-            }
-        }
-        
+        TimeSpan time = TimeSpan.FromSeconds(countdownTime);
+        string s = string.Format("{0:D2}:{1:D2}",time.Minutes,time.Seconds);
+        countdownDisplay.text = s;
     }
 
     public void SetcountdownTime(float time) {
