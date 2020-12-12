dca0269f030a7b83a211f4edafe25899ae4303a3
Matovita <mateusz.dela.md@gmail.com>
2020-12-12 17:38:37 +0100

    Everything fixed and working :P

---
+++ Assets/MinimapScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MinimapScript : MonoBehaviour {
+
+    private GameObject player;
+    private float x, y;
+
+    void Start() {
+        player = GameObject.Find("playerBoatBlue");
+        x = gameObject.GetComponent<Transform>().position.x;
+        y = gameObject.GetComponent<Transform>().position.y;
+    }
+
+    void Update() {
+        ChangePos();
+    }
+
+    void ChangePos() {
+        gameObject.GetComponent<Transform>().position = new Vector3(x + player.transform.position.x, y + player.transform.position.y, -10);
+    }
+}
--- Assets/Scripts/Quest/QuestGiverScript.cs
+++ Assets/Scripts/Quest/QuestGiverScript.cs
@@ -22,18 +22,27 @@ public class QuestGiverScript : MonoBehaviour {
     private void StringFillerPorts(int i, PortScript port) {
         switch (i) {
             case 0: {
-                port.quest.title = "Save Us";
-                port.quest.description = "later";
-                port.quest.reward = "Reward: ship 1";
-                port.quest.completed = "Thanks!";
+                port.quest.title = "Priate ship";
+                port.quest.description = "The rumor has it that not far to the north, there is a small pirate ship plundering nerby ports and islands. Please do something with it!";
+                port.quest.reward = "Reward: ship 1 and horde mode on";
+                port.quest.completed = "Thank you very much! But watch out for yourself now. You have angered whole horde of priates and they WILL come for you!";
                 break;
             }
             case 1: {
-               port.quest.title = "later";
-               port.quest.description = "New ship";
-               port.quest.reward = "Reward: ship 1";
-               port.quest.completed = "Thanks!";
-               break;
+                port.quest.title = "Plank exchange";
+                port.quest.description = "We have an amazing offer for you waiting in our port. For only 15 planks we are willing to build a new ship for you! I have heard " +
+                        "that you can find some on the island to the east of our port.";
+                port.quest.reward = "Reward: ship 1";
+                port.quest.completed = "It is a plasure doing business with you!";
+                break;
+            }
+            case 2: {
+                port.quest.title = "Destroy the horde";
+                //port.quest.description = "Oooh no! It is coming, the biggest pirate ship on entire sea and it is coming for you for what you have done to the rest of them!";
+                port.quest.description = "Try do destroy as many ships as you can! So they will not trouble good people of the seas. Five should be enough :D";
+                port.quest.reward = "Reward: Victory and peace!";
+                port.quest.completed = "Impossible! You have done it, you have achived something no one even imagined to achive. Congratulations!";
+                break;
             }
         }
     }
@@ -47,8 +56,8 @@ public class QuestGiverScript : MonoBehaviour {
     private void StringFillIslands(int i, IslandScript island) {
         switch(i) {
             case 0: {
-                island.desc = "Test1";
-                island.rewa = "Reward: plank 15";
+                island.desc = "As your ship docks to the as it appears to be deserted island, you can see couple of tress which you decide to cut down.";
+                island.rewa = "You man did a great job and you collected 15 planks.";
                 break;
             }
         }
